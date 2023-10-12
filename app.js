92f5f9ce3f6cff5baa62972a4138f5b39e5a8293
rashihd <hakimrashid207@gmail.com>
2023-10-12 12:04:30 +0530

    commit on 002

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/test',(req,res)=>{
 let n1,n2;
 n1=7;
 n2=9;
-return res.status(200).send({result:`${n1+n2}`})
+return res.status(200).send({result:`${n1-n2}`})
 })
 
 module.exports = app;
