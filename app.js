33d8c8e4112c6b8f8049cbd5b2e733993d0a1e04
rashihd <hakimrashid207@gmail.com>
2023-10-12 11:51:29 +0530

    added an api /test

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Health check ok from dev branch');
+   return res.status(200).send('Health check ok from dev branch');
 });
+
+app.get('/test',(req,res)=>{
+let n1,n2;
+n1=7;
+n2=9;
+return res.status(200).send({result:`${n1+n2}`})
+})
 
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -8,3 +8,12 @@ describe('GET /', () => {
         expect(response.text).toBe('Health check ok from dev branch');
     });
 });
+
+
+describe('GET /test', () => {
+    it('should return the sum of n1 and n2', async () => {
+        const res = await request(app).get('/test');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual({ result: '16' });  // Since 7 + 9 = 16
+    });
+});
