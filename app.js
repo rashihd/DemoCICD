c5078e2bd49b9930f606f9a1040ca17ed9f1fbed
rashihd <hakimrashid207@gmail.com>
2023-10-12 11:10:37 +0530

    2nd commit on dev branch

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 
 app.get('/',(req,res)=>{
-res.status(200).send('Health check ok')
+res.status(200).send('Health check ok from dev branch')
 })
 
 app.listen(3000,()=>{
